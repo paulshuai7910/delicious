0d26b8311c1b96d5ca9350b8939f53b1ddee7363
Paul.Shuai <2900717209@qq.com>
2024-09-13 22:51:39 +0800

    fix: add profile

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React, { Profiler } from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root")
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
+  const root = ReactDOM.createRoot(rootElement)
+  const onRender = (id: any, phase: any, actualDuration: any) => {
+    console.log(
+      `Rendered ${id} in phase ${phase} with duration ${actualDuration} ms`
+    )
+  }
+
   root.render(
     <React.StrictMode>
-      <App />
+      <Profiler id="Sidebar" onRender={onRender}>
+        <App />
+      </Profiler>
     </React.StrictMode>
-  ); // 假设你有一个名为App的React组件
+  ) // 假设你有一个名为App的React组件
 } else {
-  console.error('Element with id "root" not found');
+  console.error('Element with id "root" not found')
   // 你可以在这里添加其他错误处理逻辑
 }
